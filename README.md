25bef4a9cc39606f097228bd92fedcbe1871d847
kass8mal8 <kassimaly21@gmail.com>
2022-05-13 22:32:50 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-x1w11n
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-x1w11n)
---
+++ src/Main.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import {faCartShopping } from '@fortawesome/free-solid-svg-icons' 
+
+
+const Main=()=>{
+  const image="https://lvcas-ca.github.io/E-commerce-product-page/images/image-product-1.jpg"
+  return (
+    <div className="m-div">
+      <div className="m-div--image">
+         <img src={image} />
+      </div>
+      <div className="m-div--cont">
+          <h2>sneakers company</h2>
+          <h3>
+            Fall Limited Edition Sneakers
+          </h3>
+          <p>
+            These low-profile sneakers are your perfect casual wear companion featuring a durable rubber outer sole, they'll withstand everything the weather can offer. 
+          </p>
+          <div className="m-div--list">
+          <ul>
+             <li>$125.00</li>
+             <li>50%</li>
+             <li>$250.00</li>
+          </ul>
+          </div>
+          <button><FontAwesomeIcon icon={faCartShopping} className="m-div-i"></FontAwesomeIcon>add to cart</button>
+      </div>
+    </div>
+  )
+}
+export default Main
