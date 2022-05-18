e2ad5bb6248ca91992ed7392f49ef2d14c1022dc
kass8mal8 <kassimaly21@gmail.com>
2022-05-18 19:45:33 +0300

    2 files modified

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,13 +1,20 @@
 import React, {useState} from 'react'
-const ImageSlider=({images})=>{
+const ImageSlider=()=>{
   const  [slideIndex, setSlideIndex] =useState(1)
+const images=[
+  {src:"https://lvcas-ca.github.io/E-commerce-product-page/images/image-product-1.jpg",id:1}, 
+  {src:"https://ahmed2398.github.io/Responsive-landing-page-For-products-sale/ecommerce-product-page-main/images/image-product-2.jpg",id:2},
+  {src:"https://ahmed2398.github.io/Responsive-landing-page-For-products-sale/ecommerce-product-page-main/images/image-product-3.jpg",id:3},
+  {src:"https://ahmed2398.github.io/Responsive-landing-page-For-products-sale/ecommerce-product-page-main/images/image-product-4.jpg",id:4}
+
+] 
   return(
     <div>
       {images.map(image=>
         (
-          <div className={image.id  === slideIndex ?  'slide active' : 'slide'}key={image.id}>
-          {image.id === slideIndex && (<img src={image.src} />)}
-          </div>
+          <div className={slideIndex === index ?  'slide active' : 'slide'}key={index}>
+          {slideIndex === index && (<img src={index+1} />)}
+                    </div>
         )
         )}
     </div>
--- src/Main.js
+++ src/Main.js
@@ -27,18 +27,13 @@ const Main=()=>{
     
   }
 
-const images=[
-  {src:"https://lvcas-ca.github.io/E-commerce-product-page/images/image-product-1.jpg",id:1}, 
-  {src:"https://ahmed2398.github.io/Responsive-landing-page-For-products-sale/ecommerce-product-page-main/images/image-product-2.jpg",id:2},
-  {src:"https://ahmed2398.github.io/Responsive-landing-page-For-products-sale/ecommerce-product-page-main/images/image-product-3.jpg",id:3},
-  {src:"https://ahmed2398.github.io/Responsive-landing-page-For-products-sale/ecommerce-product-page-main/images/image-product-4.jpg",id:4}
-]
+
 
   return(
     <div className="m-div">
     
       <div className="m-div--image">
-        <ImageSlider images={images} />
+        <ImageSlider />
         <div className="m-div--image-icons">
         <ul>
           <li>
