b76692a0f4a99ccf7083d51bbc8cd18c9ac2c134
kass8mal8 <kassimaly21@gmail.com>
2022-06-05 07:33:14 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React,{useState} from "react";
 import "./style.css";
 
 import Header from './Header'
 import Main from './Main'
 export default function App() {
+  const [count, setCount ]=useState (0)
   return (
     <div>   
-      <Header />
+      <Header count={count} setCount={setCount }/>
     
     </div>
   );
